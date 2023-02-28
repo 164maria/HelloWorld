de5145d1d371af58e3c781e15526c4e783d8b91e
Daniela Crăciun <daniela.craciun@totem.com.ro>
2023-02-28 14:44:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/utilities/Constants.java
@@ -0,0 +1,12 @@
+package com.example.helloworld.utilities;
+
+public class Constants {
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_PREFERENCE_NAME = "chatPreference";
+    public static final String KEY_IS_SIGNED_IN = "isSignedIn";
+    public static final String KEY_USER_ID = "userId";
+    public static final String KEY_IMAGE = "image";
+}
